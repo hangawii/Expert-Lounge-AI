d7d87ba6138e4b330034720f1a71ff3b7bff1ea5
hangawii <hangawii@gmail.com>
2026-01-25 14:57:24 +0900

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -1,5 +1,7 @@
+import React from 'react';
+
 // This file is deprecated. The main entry point is Application.tsx.
-// We provide a dummy component here to satisfy build tools that may still look for App.tsx.
+// We provide a dummy component here to satisfy any build tools tracking App.tsx.
 
 export default function App() {
   return null;
